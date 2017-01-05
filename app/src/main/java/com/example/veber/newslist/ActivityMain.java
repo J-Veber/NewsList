6ac29f3c969443c038e32a5af18e8e1259dffe12
J-Veber <kovalsky.corp@gmail.com>
2017-01-05 22:12:34 +1000

    initial commit

--- app/src/main/java/com/example/veber/newslist/ActivityMain.java
+++ app/src/main/java/com/example/veber/newslist/ActivityMain.java
@@ -3,11 +3,23 @@ package com.example.veber.newslist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONException;
+
+import cz.msebera.android.httpclient.Header;
+
+
 public class ActivityMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NewsRestClientUsage _newsRestClientUsage = new NewsRestClientUsage();
+        try {
+            _newsRestClientUsage.getNews();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/example/veber/newslist/NewsRestClient.java
@@ -0,0 +1,23 @@
+package com.example.veber.newslist;
+import com.loopj.android.http.*;
+/**
+ * Created by Veiber on 05.01.2017.
+ */
+
+public class NewsRestClient {
+    private static final String BASE_URL = "https://newsapi.org/v1/articles";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
