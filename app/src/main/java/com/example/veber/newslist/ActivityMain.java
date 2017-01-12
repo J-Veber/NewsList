4e22dee9676954d4ff301c94b3f2f7a2cb9bcf79
J-Veber <kovalsky.corp@gmail.com>
2017-01-13 08:45:01 +1000

    ImageLoader

--- app/src/main/java/com/example/veber/newslist/ActivityMain.java
+++ app/src/main/java/com/example/veber/newslist/ActivityMain.java
@@ -1,9 +1,14 @@
 package com.example.veber.newslist;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import org.json.JSONException;
 
@@ -17,6 +22,18 @@ public class ActivityMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton _menuIB;
+        TextView _authorTV;
+        TextView _dateTV;
+        TextView _titleTV;
+        ImageView _newspicIV;
+
+        _menuIB = (ImageButton)findViewById(R.id.imageButton);
+        _authorTV = (TextView)findViewById(R.id.textView_author);
+        _dateTV = (TextView)findViewById(R.id.textView_date);
+        _titleTV = (TextView)findViewById(R.id.textView_NewsTitle);
+        _newspicIV = (ImageView)findViewById(R.id.imageView_NewsImage);
+
         NewsRestClientUsage _newsRestClientUsage = new NewsRestClientUsage();
         try {
             _newsRestClientUsage.getNews();
@@ -24,9 +41,13 @@ public class ActivityMain extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        String json;
+        String json = "";
         //AllNews _allnews = new AllNews();
         Gson gson = new Gson();
         AllNews _allnews = gson.fromJson(json, AllNews.class);
+
+        ImageLoader _imageLoader = ImageLoader.getInstance();
+        //_imageLoader.displayImage();
+
     }
 }
